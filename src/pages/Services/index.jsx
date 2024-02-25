320c3f8059f0f7ed8b6556328ac1d355ab197e0f
FidanHuseynzade <huseynzadefidan7@gmail.com>
2024-02-26 01:38:01 +0400

    done

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -10,50 +10,50 @@ const Services = () => {
       <div className="container">
         <div className="row">
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Beauty Massage</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Facial Therapy</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Skin Care</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Parlor & Beauty</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Eye Shadow</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Relaxation Room</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>MakeUp</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
           <div className="col col-xl-3 col-sm-6 col-12">
-            <img src="../../../public/icon.png" alt="" />
+            <img src="https://www.svgrepo.com/show/519682/testbook.svg" alt="" />
             <h3>Aroma Therapy</h3>
             <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
           </div>
         </div>
       </div>
       <div className="app">
-      <h3>Discover the Best Beauty Salon Center</h3>
-      <button className='button1'>MAKE AN APPOINTMENT</button>
+        <h3>Discover the Best Beauty Salon Center</h3>
+        <button className='button1'>MAKE AN APPOINTMENT</button>
       </div>
     </div>
   )
